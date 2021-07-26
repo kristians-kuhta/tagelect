558a31f78b86c3c3f38218a2ae98879bf9dd2058
Kristians Kuhta <kristians.kuhta@gmail.com>
2021-07-26 23:26:56 +0300

    Add webpack configuration file
    * use minimization (Terser)
    * load SCSS with sass-loader

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+	test: /\.s[ac]ss$/i,
+	use: [
+	  // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+	]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
